d91f7c19e6a2fb301568634bcd1f4a0201d6f6e7
Al'khan <201731@astanait.edu.kz>
2022-04-05 16:03:36 +0600

    finished coffee page, add scrollReveal animation

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+const menuIcon = document.querySelector('#hiddenMenu')
+const hiddenMenu = document.querySelector('#hiddenMenu > ul')
+
+const goTopBtn = document.querySelector('#goTopBtn')
+
+ScrollReveal().reveal('div', {delay: 25, reset: true})
 feather.replace()
 
 // const swiper = new Swiper('.swiper', {
@@ -11,9 +17,27 @@ feather.replace()
 //     }
 // })
 
-const menuIcon = document.querySelector('#hiddenMenu')
-const hiddenMenu = document.querySelector('#hiddenMenu > ul')
-
 menuIcon.addEventListener('click', e => {
     hiddenMenu.classList.toggle('hide')
+})
+
+const goTopBtnAnimation = () => {
+    if (window.scrollY <= window.innerHeight / 2) {
+        goTopBtn.classList.add('hide')
+        setTimeout(() => {
+            goTopBtn.style.opacity = '0'
+        }, 500)
+    } else {
+        goTopBtn.classList.remove('hide')
+        setTimeout(() => {
+            goTopBtn.style.opacity = '1'
+        }, 500)
+    }
+}
+goTopBtnAnimation()
+
+window.addEventListener("scroll", goTopBtnAnimation)
+
+goTopBtn.addEventListener('click', () => {
+    window.scrollTo(0, 0)
 })
